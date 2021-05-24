2e67ca5e13dea1517ffab09a692576c66d5aac67
Kameleoon <jnrivasseau@kameleoon.com>
2021-05-24 16:36:27 +0300

    2.0.5 release

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "kameleoonClient",
-            url: "https://github.com/Kameleoon/client-swift/releases/download/2.0.4/kameleoon-client-swift-2.0.4.zip",
-            checksum: "644c873b6d8b546eda90355d946947bd938ce018d2aab9f5f33a85799332b39b"
+            url: "https://github.com/Kameleoon/client-swift/releases/download/2.0.4/kameleoon-client-swift-2.0.5.zip",
+            checksum: "7615cf61bd18ff55202b5d32564b5ca56653b73bc66f1fdac549b4c5f01f21d8"
         )
     ])
