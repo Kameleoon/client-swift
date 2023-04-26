b80e46d20160509af0ebcd5de4f5ad9adc1caeb2
Dmitry Eryshov <sdk@kameleoon.com>
2023-04-26 19:29:43 +0600

    iOS (Swift) SDK 3.0.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "kameleoonClient",
-            url: "https://github.com/Kameleoon/client-swift/releases/download/3.0.0/kameleoon-client-swift-3.0.0.zip",
-            checksum: "2ab2a8f5052da6bd193197894799a64daa5df9938cb8ee0900f322734ff561e7"
+            url: "https://github.com/Kameleoon/client-swift/releases/download/3.0.1/kameleoon-client-swift-3.0.1.zip",
+            checksum: "a148ac33336ec346a15a0865e0e40eea11dd692b0c01ea2e8fb6ea27e9782368"
         )
     ])
